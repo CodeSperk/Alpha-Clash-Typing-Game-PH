5b233b51a04998efa935651de42730c7d81f9384
Mahbubur Rahman <inbx.mahbub@gmail.com>
2024-02-16 18:56:09 +0600

    randomly genereted alphabet displyed

--- script.js
+++ script.js
@@ -6,30 +6,34 @@ function showElementById(elementId){
   const element = document.getElementById(elementId);
   element.classList.remove('hidden');
 }
-function setElementTextById(elementId, value){
-  const element = document.getElementById(elementId);
-  element.innerText = value;
-}
+
 
 function generateARandomAlphabet(){
   const alphabets = "abcdefghijklmnopqrstuvwxyz";
   const alphabetArray = alphabets.split('');
 
-  //random number index from 1 to 26
-  const randomNumber = Math.random()*26;
+  //random number index from 0 to 25
+  const randomNumber = Math.random()*25;
   const index = Math.round(randomNumber);
   const randomAlphabet = alphabetArray[index];
 
   //set random alphabet in the display
-  setElementTextById("displayed-alphabet", randomAlphabet)
   return randomAlphabet;
 }
-generateARandomAlphabet()
 
+function continueGame(){
+  const alphabet = generateARandomAlphabet();
 
+  //set random alphabet
+  const alphaElement = document.getElementById('displayed-alphabet');
+  alphaElement.innerText = alphabet;
+}
 
 
 function playNow(){
   hideElementById('home-section');
   showElementById('playground-section');
+
+  continueGame();
 }
+
