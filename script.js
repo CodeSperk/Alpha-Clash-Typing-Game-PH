591ef6b75e59b6108f298ffc33bd844190308b59
Mahbubur Rahman <inbx.mahbub@gmail.com>
2024-02-16 18:27:47 +0600

    generated & set  random alphabet

--- script.js
+++ script.js
@@ -6,6 +6,26 @@ function showElementById(elementId){
   const element = document.getElementById(elementId);
   element.classList.remove('hidden');
 }
+function setElementTextById(elementId, value){
+  const element = document.getElementById(elementId);
+  element.innerText = value;
+}
+
+function generateARandomAlphabet(){
+  const alphabets = "abcdefghijklmnopqrstuvwxyz";
+  const alphabetArray = alphabets.split('');
+
+  //random number index from 1 to 26
+  const randomNumber = Math.random()*26;
+  const index = Math.round(randomNumber);
+  const randomAlphabet = alphabetArray[index];
+
+  //set random alphabet in the display
+  setElementTextById("displayed-alphabet", randomAlphabet)
+  return randomAlphabet;
+}
+generateARandomAlphabet()
+
 
 
 
